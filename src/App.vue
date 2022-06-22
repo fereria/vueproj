4103574f01bc2e12e20cf0a58badb140fbbc9800
Megumi Ando <remiria@flame-daybreak.net>
2022-06-23 00:20:59 +0900

    sampleproj

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-treeview rounded hoverable activatable :items="items"></v-treeview>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	data: () => ({
+		items: [
+			{
+				id: 1,
+				name: "Applications :",
+				children: [
+					{ id: 2, name: "Calendar : app" },
+					{ id: 3, name: "Chrome : app" },
+					{ id: 4, name: "Webstorm : app" },
+				],
+			},
+			{
+				id: 5,
+				name: "Documents :",
+				children: [
+					{
+						id: 6,
+						name: "vuetify :",
+						children: [
+							{
+								id: 7,
+								name: "src :",
+								children: [
+									{ id: 8, name: "index : ts" },
+									{ id: 9, name: "bootstrap : ts" },
+								],
+							},
+						],
+					},
+					{
+						id: 10,
+						name: "material2 :",
+						children: [
+							{
+								id: 11,
+								name: "src :",
+								children: [
+									{ id: 12, name: "v-btn : ts" },
+									{ id: 13, name: "v-card : ts" },
+									{ id: 14, name: "v-window : ts" },
+								],
+							},
+						],
+					},
+				],
+			},
+			{
+				id: 15,
+				name: "Downloads :",
+				children: [
+					{ id: 16, name: "October : pdf" },
+					{ id: 17, name: "November : pdf" },
+					{ id: 18, name: "Tutorial : html" },
+				],
+			},
+			{
+				id: 19,
+				name: "Videos :",
+				children: [
+					{
+						id: 20,
+						name: "Tutorials :",
+						children: [
+							{ id: 21, name: "Basic layouts : mp4" },
+							{ id: 22, name: "Advanced techniques : mp4" },
+							{ id: 23, name: "All about app : dir" },
+						],
+					},
+					{ id: 24, name: "Intro : mov" },
+					{ id: 25, name: "Conference introduction : avi" },
+				],
+			},
+		],
+	}),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
