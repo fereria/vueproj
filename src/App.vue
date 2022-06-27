f855fb151e55c43ad996c6441122550b63b2e937
Megumi Ando <remiria@flame-daybreak.net>
2022-06-27 22:36:04 +0900

    コンポーネントを作る

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
 <template>
 	<div>
-		<p>{{ sample }}</p>
 		<div v-for="(item, index) in items" :key="index">
 			<input
 				type="radio"
 				name="sample1"
 				:value="item"
 				v-model="itemValue"
+				@change="sampleB"
 			/>
 			{{ item }}
 		</div>
+		<hello-world :childComp="itemValue" @add="add1" />
+		{{ count }}
 	</div>
 </template>
 
 <script>
+import HelloWorld from "./components/HelloWorld.vue";
+
 export default {
+	components: { HelloWorld },
 	data() {
 		return {
 			itemValue: "りんご",
+			count: 0,
 			items: ["ぶどう", "りんご", "みかん"],
 		};
 	},
 	computed: {
 		sample() {
 			return "選択中: " + this.itemValue;
+		},
+	},
+	filters: {},
+	methods: {
+		sampleB() {},
+		add1(count) {
+			this.count = count;
 		},
 	},
 };
